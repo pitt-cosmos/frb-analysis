adf7fb9dcc4720acc8a1971f60879c4b0ba5a9da
Yilun Guan <zoom.aaron@gmail.com>
2017-05-27 15:43:32 -0400

    Add get_unique_detector_list function

--- find_frb.py
+++ find_frb.py
@@ -62,20 +62,48 @@ def overlap_detectors_generator(array):
     overlap_dets = [None] * len(arx) # Generate an empty list to store adjacent detector lists
     for i in range(len(arx)):
         dis = (arx - arx[i])**2 + (ary - ary[i])**2
-        _mask =  dis == 0 # May need to give a range, and it will contain the item itself
+        _mask =  (dis == 0) & (det_type == 'tes') # May need to give a range, and it will contain the item itself
         mask = _mask.flatten()
         indexes = np.where(mask == True) 
         overlap_location_list = list(list(indexes)[0]) # Normalize the np array output to list
-        overlap_location_list.remove(i) # Need to remove i from the list
+        if i in overlap_location_list:
+            overlap_location_list.remove(i) # Need to remove i from the list 
+
         overlap_dets[i]= overlap_location_list
     # Generate a function to access the data to make sure above procedures run once only
     def get_overlap_detectors(detector):
-        return overlap_dets[detector]
+        if overlap_dets[detector]==[]:
+            return None
+        else:
+            return overlap_dets[detector][0]
     
     return get_overlap_detectors
+
+def get_unique_detectors(array):
+    '''
+    Function to find a unique list of detectors that occupy each 
+    detector location
+    '''
+    assert array == 'AR1' or array == 'AR2' 
+    amdata = moby2.scripting.get_array_data({'season':'2014', 'array_name': array})
+    _mask = (amdata['det_type'] == 'tes')
+    mask = _mask.flatten()
+    indexes = np.where(mask == True)
+    input_list = list(list(indexes)[0]) 
+    output_list = list(input_list) # Make a copy to input list 
+    get_overlap_detectors = overlap_detectors_generator(array)
+ 
+    for i in input_list:
+        i_overlap = get_overlap_detectors(i)
+        # i_overlap > i is to make sure no double removal occur
+        if i_overlap != None and (i_overlap in output_list) and i_overlap > i:
+            output_list.remove(i_overlap)
+    
+    return output_list
 
 find_adjacent_detectors = adjacent_detectors_generator('AR2')
 find_overlap_detectors = overlap_detectors_generator('AR2')
 print find_adjacent_detectors(10)
-find_overlap_detectors(10)
 print find_overlap_detectors(10)
+print get_unique_detectors('AR2')
+
