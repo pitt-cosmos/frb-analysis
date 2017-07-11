e90a84e6b70f40221d22fdca037a6500050021bf
Yilun Guan <zoom.aaron@gmail.com>
2017-07-11 16:20:44 -0400

    Change cut file structure from json to cPickle with metadata added

--- find_cuts.py
+++ find_cuts.py
@@ -3,17 +3,15 @@ import numpy as np
 import sys
 from moby2.instruments import actpol
 from moby2.scripting import get_filebase
+import cPickle as pickle
 
 #Objects that allow access to moby2 data. See .moby2 for details
 db = actpol.TODDatabase()
 ids = db.select_tods()
 fb = moby2.scripting.get_filebase()
 
-#Empty dictionary and loop variables
-meta = {}
-ld = 0
-i = 0
 
+glitchp ={ 'nSig': 10, 'tGlitch' : 0.007, 'minSeparation': 30, 'maxGlitch': 50000, 'highPassFc': 6.0, 'buffer': 0 }
 #The outer loop iterates through every TOD while the inner loop iterates
 #through every set of cuts in the n'th TOD. Not every TOD has cuts in the
 #depot however, so it lets you know if one of the TODs cannot be analyzed in
@@ -23,7 +21,6 @@ i = 0
 start = int(sys.argv[1])
 end = int(sys.argv[2])
 for n in range(start, end):
-    meta = {}
     try:
         tod_name = ids[n].basename
         array = ids[n].array
@@ -36,30 +33,12 @@ for n in range(start, end):
         # signals that we should look at (unbuffered)
 
         # Define glitch parameters -> note buffer = 0
-        glitchp ={ 'nSig': 10, 'tGlitch' : 0.1, 'minSeparation': 30, 'maxGlitch': 50000, 'highPassFc': 6.0, 'buffer': 0 }
         cuts = moby2.tod.get_glitch_cuts(tod=data, params=glitchp)
         print("Get cuts success")
-        #list that tores the numbers of all live detectors in a given TOD
-        lds = cuts.get_uncut()      
-        #The first loop that iterates through every cut in a live detector and appends
-        #them to a list that will be added to "meta"
-        print("Generating list of cuts")
 
-        for ld in lds:
-            #temp_list = np.empty((0))
-            cut = cuts.cuts[ld]
-
-            #Dictionary entry for detector "ld"
-            if len(cut) != 0:
-                meta[ld] = {
-                    'array' : str(array),
-                    'cuts' : cut.tolist()
-                }
-        #Writes everything to a file. Depending on how many TOD's you chose, this will
-        #either be modest, or gargantuan in size. +1 internets if you run the whole shabang
-        s = json.dumps(meta)
-        with open("outputs/" + str(n) + ".txt", "w") as f:
-            f.write(s)
+        # Save into pickle file
+        meta = {"array": array, "TOD": tod_name, "glitch_param": glitchp, "cuts": cuts}
+        pickle.dump(meta, open("outputs/" + str(n) + ".pickle", "wb"), pickle.HIGHEST_PROTOCOL)
         print("File for TOD " + str(n) + " written successfully")
     except Exception as e:
         if type(e) == IOError:
