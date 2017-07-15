551d1a201a214ba5ca56cb1e11176a67c8edbc31
Yilun Guan <yguan@Gracies-MacBook-Pro.local>
2017-07-15 09:32:18 +0200

    Add plot cuts"

--- plot_tod.py
+++ plot_tod.py
@@ -15,14 +15,21 @@ fb = get_filebase()
 TOD = 117
 DET = 33
 
-# Plot
+# Plot TOD
 tod = ids[TOD]
 tod_name = tod.basename
 print tod_name
 tod_dir = fb.filename_from_name(tod_name, single=True)
 # The role of repair pointing is not sure
 data =moby2.scripting.get_tod({'filename':tod_dir, 'repair_pointing': True})
-print data.nsamps
 #plt.plot(data.ctime, data.data[DET])
-plt.plot(data.data[DET])
+plt.plot(data.ctime, data.data[DET])
+
+
+# Plot cuts together with TOD
+glitchp ={ 'nSig': 10, 'tGlitch' : 0.007, 'minSeparation': 30, 'maxGlitch': 50000, 'highPassFc': 6.0, 'buffer': 0 }
+
+cuts = moby2.tod.get_glitch_cuts(tod=data, params=glitchp)
+_mask = cuts.cuts[DET].get_mask()
+plt.plot(data.ctime[_mask], data.data[DET][_mask])
 plt.show()
