f90d1e09446caa9f0d5654325404be2c7555d144
Yilun Guan <zoom.aaron@gmail.com>
2017-07-19 01:18:28 -0400

    Change plot_tod

--- plot_tod.py
+++ plot_tod.py
@@ -12,8 +12,9 @@ ids = db.select_tods()
 fb = get_filebase()
 
 # Selection
-TOD = 117
-DET = 33
+#TOD = 117
+TOD = 2000
+DET = 50
 
 # Plot TOD
 tod = ids[TOD]
@@ -23,13 +24,16 @@ tod_dir = fb.filename_from_name(tod_name, single=True)
 # The role of repair pointing is not sure
 data =moby2.scripting.get_tod({'filename':tod_dir, 'repair_pointing': True})
 #plt.plot(data.ctime, data.data[DET])
-plt.plot(data.ctime, data.data[DET])
 
 
 # Plot cuts together with TOD
 glitchp ={ 'nSig': 10, 'tGlitch' : 0.007, 'minSeparation': 30, 'maxGlitch': 50000, 'highPassFc': 6.0, 'buffer': 0 }
 
-cuts = moby2.tod.get_glitch_cuts(tod=data, params=glitchp)
+#cuts = moby2.tod.get_glitch_cuts(tod=data, params=glitchp)
+#cuts = moby2.tod.get_mce_cuts(data)
+cuts = moby2.scripting.get_cuts({'depot':'/mnt/act3/users/lmaurin/depot', 'tag':'MR1_PA2_2014'},tod=data)
 _mask = cuts.cuts[DET].get_mask()
-plt.plot(data.ctime[_mask], data.data[DET][_mask])
+print cuts.get_cut()
+#plt.plot(data.ctime[_mask], data.data[DET][_mask], 'r-')
+#plt.plot(data.data[DET][~_mask], 'r-')
 plt.show()
