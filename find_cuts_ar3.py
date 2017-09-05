d7c7f731a8b29e36ce1c7be497cbdc78cdff3370
bmalumphy <bjm126@pitt.edu>
2017-09-05 13:45:27 -0400

    Update to use Dictionary Format for new params

--- find_cuts_ar3.py
+++ find_cuts_ar3.py
@@ -1,63 +1,62 @@
-import moby2, json
+import moby2, cPickle
 import numpy as np
-import sys
 from moby2.instruments import actpol
 from moby2.scripting import get_filebase
-from moby2.tod.cuts import CutsVector
-import cPickle as pickle
 
-#Objects that allow access to moby2 data. See .moby2 for details
 db = actpol.TODDatabase()
 ids = db.select_tods()
-fb = moby2.scripting.get_filebase()
+fb = get_filebase()
 
-glitchp ={ 'nSig': 10, 'tGlitch' : 0.007, 'minSeparation': 30, 'maxGlitch': 50000, 'highPassFc': 6.0, 'buffer': 0 }
-#The outer loop iterates through every TOD while the inner loop iterates
-#through every set of cuts in the n'th TOD. Not every TOD has cuts in the
-#depot however, so it lets you know if one of the TODs cannot be analyzed in
-#this way.
-#for n in range(0, len(ids)):
-#+Contro the start and end with commandline arguments for convenence
-start = int(sys.argv[1])
-end = int(sys.argv[2])
-for n in range(start, end):
+meta = {}
+n = 0
+ardata = moby2.scripting.get_array_dta({'season':'2016', 'array_name': 'AR3'})
+
+glitchp = {'nSig':10, 'tGlitch':0.007, 'minSeparation': 30, 'maxGlitch': 50000,
+    'highPassFc': 6.0, 'buffer':0}
+
+for n in range(0, len(ids)):
     try:
         tod_name = ids[n].basename
-        array = ids[n].array
         print(str(n) + ': ' + tod_name)
         tod_dir = fb.filename_from_name(tod_name, single = True)
         print("Get TOD dir success")
-        data = moby2.scripting.get_tod({'filename': tod_dir, 'repair_pointing': True})
+        data = moby2.scripting.get_tod({'filename':tod_dir, 'repair_pointing': True})
         print("Get TOD success")
-        # For the purpose of finding frb signals, it's partial glitch
-        # signals that we should look at (unbuffered)
-
-        # Define glitch parameters -> note buffer = 0
-        # Test if detrend and remove_mean affect the tod
-        #moby2.tod.remove_mean(data)
-        #moby2.tod.detrend_tod(data) 
-
-        # Calibration
-#        cal = moby2.scripting.get_calibration({'_execcfg': '{cfglib}/defaults.in:cal_iv_pw'}, tod=data)
-#        data.data *= cal.cal[:,None]        
-       
-        # Add up signals from different polarization
-        cuts = moby2.tod.get_glitch_cuts(tod=data, params=glitchp)
-        skim_cuts = [None] * len(cuts.cuts)
         
+        cuts = moby2.tod.get_glitch_cuts(tod = data, params = glitchp)
         print("Get cuts success")
-        for i in range(len(cuts.cuts)):
-            skim_cuts[i] = CutsVector([cut for cut in cuts.cuts[i] if cut[1] - cut[0] < 20])
-            
-#        print skim_cuts
-        # Save into pickle file
-        cuts.cuts = skim_cuts
-        pickle.dump(cuts, open("outputs/" + str(n) + ".cut", "wb"), pickle.HIGHEST_PROTOCOL)
+        
+        print("Generating list of cuts")
+        
+        for det in range(0,1055):
+            cut_array = np.empty((0))
+            cut = cuts.cuts[det]
+            i = 0
+            while i < len(cut):
+                calc = cuts.cuts[det][i][1]-cuts.cuts[det][i][0]
+                
+                if calc <= 20:
+                    cut_array = np.append(cut_array, [cuts.cuts[det][i]])
+                i +=1
+                
+            a = cut_array.reshape((len(cut_array)/2, 2)).tolist()
+            pol_family = ardata['pol_family']
+            nom_freq = ardata['nom_freq']
+            if cut_array != np.empty((0)):
+                meta[det] = {
+                    'det_id' : det,
+                    'cuts' : a,
+                    'freq_channel' : nom_freq[det],
+                    'pol_channel' : pol_family[det],
+                    'TOD_name' : tod_name
+                }
+        with open("outputs/" + str(n) + ".txt", "wb") as f:
+            cPickle.dump(meta, fb)
         print("File for TOD " + str(n) + " written successfully")
     except Exception as e:
         if type(e) == IOError:
             print("TOD " + str(n) + " has no cuts!")
-        elif type(e) == TypeError:
+        if type(e) == TypeError:
             print("TOD " + str(n) + " file cannot be found!")
         else:
             print(e)
