2d9fdbb09da061df9bc22d44befddfbcf23a987d
bmalumphy <bjm126@pitt.edu>
2017-09-09 14:17:01 -0400

    Update slurm_job.sh

--- slurm_job.sh
+++ slurm_job.sh
@@ -6,11 +6,10 @@
 #SBATCH -t 90:00:00                        # 90 hours walltime
 #SBATCH --mem=4000MB                       # memory in MB 
 #SBATCH -p act
-#SBATCH --output=logs/5ms.out                # file for STDOUT 
-#SBATCH --mail-user=yig20@pitt.edu         # Mail  id of the user 
+#SBATCH --output=logs/5ms.out              # file for STDOUT 
+#SBATCH --mail-user=bjm126@pitt.edu        # Mail  id of the user 
 #SBATCH --mail-type=end                    # Slurm will send at the completion of your job 
 
-#python find_cuts.py 300 400
-python insert_glitch.py
+python find_cuts_txt.py
 
 # end of script 
